c8b59f83ef23ea9f53f28b74dc1034f8b45dd164
Cristina Fernández Fernández <cristina.98fdz@gmail.com>
2024-07-21 21:39:34 +0200

    eliminados

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -15,30 +15,19 @@ export class ResultComponent {
   playerName: string = '';
 
   constructor(private router: Router) {
-    // Obtén la puntuación del localStorage si existe
     this.score = Number(localStorage.getItem('userScore')) || 0;
   }
 
   submitScore(): void {
-    // Asegúrate de que el nombre del jugador no esté vacío
     if (!this.playerName.trim()) {
       alert('Por favor, ingresa tu nombre.');
       return;
     }
 
-    // Recupera el ranking actual o inicializa uno vacío si no existe
     const ranking = JSON.parse(localStorage.getItem('ranking') || '[]');
-
-    // Añade el nuevo puntaje al ranking
     ranking.push({ nombre: this.playerName, puntuacion: this.score });
-
-    // Ordena el ranking por puntuación en orden descendente
     ranking.sort((a: any, b: any) => b.puntuacion - a.puntuacion);
-
-    // Guarda solo los 10 mejores puntajes en localStorage
     localStorage.setItem('ranking', JSON.stringify(ranking.slice(0, 10)));
-
-    // Navega a la página de ranking
     this.router.navigate(['/ranking']);
   }
 }
